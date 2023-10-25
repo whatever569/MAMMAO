c9f1547dee70bba15c87336bc6fe62b82b644e4b
ATI9590 <65565903+ATI9590@users.noreply.github.com>
2023-10-25 13:40:17 +0200

    Putting everything back into header files

--- Code/headers/IR.h
+++ Code/headers/IR.h
@@ -8,8 +8,41 @@
 #include "headers/PINS.h"
 int results[numUltrasonicSensors];
 
-void irSetup();
-void lineTracking();
-bool isClose();
+void irSetup()
+{
+  pinMode(irL, INPUT);
+  pinMode(irR, INPUT);
+}
+
+void lineTracking()
+{
+  ultrasonicSensorsCheck(results, ultrasonicSensorsPins[], numUltrasonicSensors, numUltrasonicSamples);
+  if (isClose(results[0], results[1], results[2]))
+  {
+    stop();
+  }
+  else if (!isClose(results[0], results[1], results[2]) && digitalRead(irL) == HIGH)
+  {
+    left();
+  }
+  else if (!isClose(results[0], results[1], results[2]) && digitalRead(irR) == HIGH)
+  {
+    right();
+  }
+  else
+  {
+    forward();
+  }
+}
+
+bool isClose(float a, float b, float c)
+{
+  if (a > 10 && b > 10 && c > 10)
+  {
+    return false;
+  }
+  else
+    return true;
+}
 
 #endif
--- Code/headers/STORAGE.h
+++ Code/headers/STORAGE.h
@@ -4,11 +4,39 @@
 #ifndef STORGAGE_H
 #define STORGAGE_H
 
+#include <EEPROM.h>
+
 // Define the EEPROM address where the total system time is stored
 const int totalSystemTimeAddress = 0;
 
+// Function to add a given time value to the total system time stored in EEPROM
+void addTimeToEepromTotalSystemTime(unsigned long time)
+{
+  // Calculate the new total system time by adding the given time to the previous total
+  unsigned long newTime = getPreviousTotalSystemTime() + time;
 
-unsigned long getPreviousTotalSystemTime();
-void addTimeToEepromTotalSystemTime(unsigned long time);
-void eraseEEPROM();
+  // Store the updated total system time back in EEPROM
+  EEPROM.put(totalSystemTimeAddress, newTime);
+
+
+}
+
+// Function to retrieve the previous total system time from EEPROM
+unsigned long getPreviousTotalSystemTime()
+{
+  unsigned long previousTotalSystemTime;
+  
+  // Read the previous total system time from EEPROM
+  EEPROM.get(totalSystemTimeAddress, previousTotalSystemTime);
+  
+  return previousTotalSystemTime;
+}
+
+// Function to erase all data stored in EEPROM (set to 0)
+void eraseEEPROM()
+{
+  // Loop through each EEPROM address and write 0 to erase the data
+  for(int i = 0; i < 1024; i++) EEPROM.write(i, 0);
+}
+
 #endif
