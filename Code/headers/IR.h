9766c59fe4ba8f8b1b781e579b9f02acda7761c8
Anubroto <144114553+Anubroto05@users.noreply.github.com>
2023-10-25 17:29:31 +0200

    Update IR.h

--- Code/headers/IR.h
+++ Code/headers/IR.h
@@ -1,22 +1,33 @@
 #ifndef IR_H
 #define IR_H
 
+//define IR pins constants
 #define irL 5
 #define irR 12
-#include "headers/ULTRASONIC.h"
-#include "headers/MOTOR.h"
-#include "headers/PINS.h"
+
+#define THRESHOLD 10
+
+// Include header file for the ultrasonic to check the distance and Motor file for the directions and pins
+#include "ULTRASONIC.h"
+#include "MOTOR.h"
+#include "PINS.h"
+
+//Decleare an array to store the reading sof the ultrasonic sensors
 int results[numUltrasonicSensors];
 
+//decleare IR sensor setup function
 void irSetup()
 {
   pinMode(irL, INPUT);
   pinMode(irR, INPUT);
 }
 
+//Function for performing the Line Tracking using the Ultrasonic and IR sesnsors simultaneously 
 void lineTracking()
 {
+  //Getting the ultrasonic sesnsor readings
   ultrasonicSensorsCheck(results, ultrasonicSensorsPins[], numUltrasonicSensors, numUltrasonicSamples);
+  //Check If any object is close based on the ultrasonic sensor readings
   if (isClose(results[0], results[1], results[2]))
   {
     stop();
@@ -34,10 +45,10 @@ void lineTracking()
     forward();
   }
 }
-
+//Function to check if theres any object close to the car based on the ultrasonic sensor readings
 bool isClose(float a, float b, float c)
 {
-  if (a > 10 && b > 10 && c > 10)
+  if (a > THRESHOLD && b > THRESHOLD && c > THRESHOLD)
   {
     return false;
   }
